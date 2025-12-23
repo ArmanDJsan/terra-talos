9feb9ddc3d6add5d15e564599646f7fe8d4dc1fa
ArmandJsan <sc.armand@gmail.com>
2025-12-23 01:45:49 -0400

    listo para ejecutar

--- main.tf
+++ main.tf
@@ -2,34 +2,37 @@ module "kubernetes" {
   source  = "hcloud-k8s/kubernetes/hcloud"
   version = "1.3.0"
 
+  # 1. Pasamos el token directamente al módulo
+  hcloud_token = var.hcloud_token
+  
   cluster_name = var.cluster_name
-  location     = var.location
 
-  # Red interna de alta velocidad
+  # 2. Versiones de software
+  talos_version      = "v1.9.6"
+  kubernetes_version = "1.31.1"
+
+  # 3. Configuración de Red
   network_ipv4_cidr = "10.0.0.0/16"
 
-  # Alta Disponibilidad: 3 Masters
+  # 4. Los Masters (CPX21)
   control_plane_nodepools = [
     {
-      name        = "control-plane",
+      name        = "master",
       server_type = "cpx21",
       size        = 3,
       location    = var.location
+      type        = "cloud"
     }
   ]
 
-  # Nodos de aplicaciones: 3 Workers
+  # 5. Los Workers (CPX31)
   worker_nodepools = [
     {
       name        = "workers",
       server_type = "cpx31",
       size        = 3,
       location    = var.location
+      type        = "cloud"
     }
   ]
-
-  # Add-ons listos para producción
-  hcloud_csi_enabled         = true # Discos
-  ingress_controller_enabled = true # Nginx
-  cert_manager_enabled       = true # SSL
 }
