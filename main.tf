f874414bbb013234ff703007dd8b15bf26f31116
ArmandJsan <sc.armand@gmail.com>
2025-12-26 15:15:31 -0400

    update

--- main.tf
+++ main.tf
@@ -2,26 +2,28 @@ module "kubernetes" {
   source  = "hcloud-k8s/kubernetes/hcloud"
   version = "3.16.0"
 
-  # Credenciales y General
   hcloud_token = var.hcloud_token
   cluster_name = var.cluster_name
-  
-  # CAMBIO: Nombres exactos para la v3.16.x
-  hcloud_location = var.location 
-  talos_image_id  = var.talos_image_id
 
-  # Configuración técnica
+  # USANDO LOS NOMBRES QUE VIMOS EN EL GREP:
+  # 1. El módulo no pide 'location' global, lo pide por cada nodepool y para el LB
   hcloud_ccm_load_balancers_location = var.location
-  talos_version                      = "v1.9.5"
-  kubernetes_version                 = "1.31.1"
 
-  # Red
+  # 2. Para usar tu Snapshot 344281184, el módulo usa esta variable:
+  packer_amd64_builder = var.talos_image_id
+
+  # 3. Versiones
+  talos_version      = "v1.9.5"
+  kubernetes_version = "1.31.1"
+
+  # 4. Red (Basado en tu grep: network_ipv4_cidr)
   network_ipv4_cidr = "10.0.0.0/16"
-  
-  # CAMBIO: En esta versión se suele usar este nombre para los accesos
-  talos_api_allowed_networks = ["0.0.0.0/0"] 
 
-  # Nodepools
+  # 5. Firewall (Basado en tu grep: firewall_talos_api_source)
+  firewall_talos_api_source = ["0.0.0.0/0"]
+  firewall_kube_api_source  = ["0.0.0.0/0"]
+
+  # 6. Nodepools (Aquí sí se define la location de los servidores)
   control_plane_nodepools = [
     {
       name     = "master",
@@ -40,7 +42,7 @@ module "kubernetes" {
     }
   ]
 
-  # Salidas
+  # 7. Archivos de salida
   cluster_kubeconfig_path  = "kubeconfig"
   cluster_talosconfig_path = "talosconfig"
 }
