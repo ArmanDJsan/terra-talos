1655d1fb0c7cb35dc5cab172ec06cef8e4f0737c
ArmandJsan <sc.armand@gmail.com>
2025-12-26 15:18:34 -0400

    update

--- main.tf
+++ main.tf
@@ -5,25 +5,22 @@ module "kubernetes" {
   hcloud_token = var.hcloud_token
   cluster_name = var.cluster_name
 
-  # USANDO LOS NOMBRES QUE VIMOS EN EL GREP:
-  # 1. El módulo no pide 'location' global, lo pide por cada nodepool y para el LB
+  # CAMBIO: Ahora pasamos un objeto en lugar de un string
+  packer_amd64_builder = {
+    image_id = var.talos_image_id
+  }
+
   hcloud_ccm_load_balancers_location = var.location
 
-  # 2. Para usar tu Snapshot 344281184, el módulo usa esta variable:
-  packer_amd64_builder = var.talos_image_id
-
-  # 3. Versiones
   talos_version      = "v1.9.5"
   kubernetes_version = "1.31.1"
 
-  # 4. Red (Basado en tu grep: network_ipv4_cidr)
   network_ipv4_cidr = "10.0.0.0/16"
 
-  # 5. Firewall (Basado en tu grep: firewall_talos_api_source)
+  # Firewall usando los nombres exactos de tu grep
   firewall_talos_api_source = ["0.0.0.0/0"]
   firewall_kube_api_source  = ["0.0.0.0/0"]
 
-  # 6. Nodepools (Aquí sí se define la location de los servidores)
   control_plane_nodepools = [
     {
       name     = "master",
@@ -42,7 +39,6 @@ module "kubernetes" {
     }
   ]
 
-  # 7. Archivos de salida
   cluster_kubeconfig_path  = "kubeconfig"
   cluster_talosconfig_path = "talosconfig"
 }
